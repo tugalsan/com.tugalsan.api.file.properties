0dfc6ed3933e2e43c7cc68d6f66113259080025c
tugalsan karabacak <tugalsan@gmail.com>
2022-07-01 09:54:28 +0300

    usage of TGS_UnSafe class

--- src/main/java/com/tugalsan/api/file/properties/server/TS_FilePropertiesUtils.java
+++ src/main/java/com/tugalsan/api/file/properties/server/TS_FilePropertiesUtils.java
@@ -5,42 +5,30 @@ import java.nio.charset.*;
 import java.nio.file.*;
 import java.util.*;
 import java.util.stream.*;
-import com.tugalsan.api.log.server.*;
 import com.tugalsan.api.pack.client.*;
 import com.tugalsan.api.stream.client.*;
-import com.tugalsan.api.string.client.*;
+import com.tugalsan.api.unsafe.client.*;
 
 public class TS_FilePropertiesUtils {
 
-    final private static TS_Log d = TS_Log.of(TS_FilePropertiesUtils.class.getSimpleName());
-
+//    final private static TS_Log d = TS_Log.of(TS_FilePropertiesUtils.class.getSimpleName());
     public static Properties createPropertyReader(Class className) {
-        try {
+        return TGS_UnSafe.compile(() -> {
             var propsName = className.getName();//NOT SIMPLE NAME
             var name = propsName.replace('.', '/').concat(".properties");
             var cl = ClassLoader.getSystemClassLoader();
             var in = cl.getResourceAsStream(name);
             if (in == null) {
-                throw new RuntimeException(TGS_StringUtils.concat(
-                        TS_FilePropertiesUtils.class.getSimpleName(),
-                        "createPropertyReader", className.getSimpleName(),
-                        "in == null"
-                ));
+                TGS_UnSafe.catchMeIfUCan(TS_FilePropertiesUtils.class.getSimpleName(), "createPropertyReader", "in == null");
             }
             var props = new Properties();
             props.load(in);
             return props;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        });
     }
 
     public static void print(Properties config) {
-        try {
-            config.store(System.out, "Loaded properties:");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        TGS_UnSafe.execute(() -> config.store(System.out, "Loaded properties:"));
     }
 
     public static String getValue(Properties source, CharSequence key) {
@@ -54,9 +42,9 @@ public class TS_FilePropertiesUtils {
     public static void setValue(Properties source, CharSequence key, CharSequence value) {
         if (value == null) {
             source.remove(key.toString());
-        } else {
-            source.setProperty(key.toString(), value.toString());
+            return;
         }
+        source.setProperty(key.toString(), value.toString());
     }
 
     public static List<String> getAllKeys(Properties source) {
@@ -97,21 +85,21 @@ public class TS_FilePropertiesUtils {
     }
 
     public static void write(Properties source, Path dest) {
-        try ( var os = Files.newOutputStream(dest);  var osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);) {
-            source.store(osw, "");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        TGS_UnSafe.execute(() -> {
+            try ( var os = Files.newOutputStream(dest);  var osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);) {
+                source.store(osw, "");
+            }
+        });
     }
 
     public static Properties read(Path source) {
-        try ( var is = Files.newInputStream(source);  var isr = new InputStreamReader(is, StandardCharsets.UTF_8);) {
-            var config = new Properties();
-            config.load(isr);
-            return config;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return TGS_UnSafe.compile(() -> {
+            try ( var is = Files.newInputStream(source);  var isr = new InputStreamReader(is, StandardCharsets.UTF_8);) {
+                var config = new Properties();
+                config.load(isr);
+                return config;
+            }
+        });
     }
 
     @Deprecated
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.tugalsan.api.file.properties {
     requires com.tugalsan.api.log;
+    requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.pack;
     requires com.tugalsan.api.stream;
     requires com.tugalsan.api.string;
