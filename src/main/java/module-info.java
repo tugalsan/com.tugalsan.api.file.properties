eb321c75f02dd57005ea5c2cb60d266101ab8445
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:03:38 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.tugalsan.api.file.properties {
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.tuple;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.stream;
     requires com.tugalsan.api.string;
     exports com.tugalsan.api.file.properties.server;
